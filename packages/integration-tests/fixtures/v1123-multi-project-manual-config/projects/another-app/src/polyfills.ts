eeaab0d3d03df5c398e3458f587087300e511f60
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:28 -0300

    improve

--- packages/integration-tests/fixtures/v1123-multi-project-manual-config/projects/another-app/src/polyfills.ts
+++ packages/integration-tests/fixtures/v1123-multi-project-manual-config/projects/another-app/src/polyfills.ts
@@ -2,24 +2,34 @@
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
  *
- * This file is divided into 2 sections:
- *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
- *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
+ * This file is divided into 3 sections:
+ *   1. Browser polyfills for old browsers. These are applied before loading ZoneJS.
+ *   2. Browser polyfills for evergreen browsers. These are applied before loading ZoneJS and are sorted by browsers.
+ *   3. Application imports. Files imported after ZoneJS that should be loaded before your main
  *      file.
  *
  * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
  * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
  * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
  *
+ * For older browsers, such as IE10 and IE11, additional polyfills are required. These are included
+ * in the first section of this file.
+ *
  * Learn more in https://angular.io/guide/browser-support
  */
 
 /***************************************************************************************************
- * BROWSER POLYFILLS
+ * BROWSER POLYFILLS FOR OLD BROWSERS
  */
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+// IE10 and IE11 requires the following for NgClass support on SVG elements
+import 'classlist.js';  // Run `npm install --save classlist.js`.
+
+// Add polyfills for older browsers here
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS FOR EVERGREEN BROWSERS
+ */
 
 /**
  * Web Animations `@angular/platform-browser/animations`
@@ -53,11 +63,12 @@
  */
 
 /***************************************************************************************************
- * Zone JS is required by default for Angular itself.
+ * ZONE JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+// Add application imports here
