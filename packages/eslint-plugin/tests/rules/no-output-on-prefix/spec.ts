b89386d8f66a995763606bbcc9d86a7c7efd1338
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:16 -0300

    improve

--- packages/eslint-plugin/tests/rules/no-output-on-prefix/spec.ts
+++ packages/eslint-plugin/tests/rules/no-output-on-prefix/spec.ts
@@ -1,12 +1,20 @@
 import { RuleTester } from '@angular-eslint/utils';
+import { FormattingType } from 'eslint';
 import rule, { RULE_NAME } from '../../../src/rules/no-output-on-prefix';
 import { invalid, valid } from './cases';
 
 const ruleTester = new RuleTester({
-  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
 });
 
 ruleTester.run(RULE_NAME, rule, {
-  valid,
-  invalid,
+  valid: valid as ReadonlyArray<[string, FormattingType?>],
+  invalid: invalid as ReadonlyArray<[string, FormattingType?, string]>,
 });
+
