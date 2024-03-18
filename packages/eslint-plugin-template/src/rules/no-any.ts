d91a244d8d7003f65cf1b28d09b7cf3758bae7c3
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:09 -0300

    improve

--- packages/eslint-plugin-template/src/rules/no-any.ts
+++ packages/eslint-plugin-template/src/rules/no-any.ts
@@ -1,4 +1,4 @@
-import type { Call } from '@angular-eslint/bundled-angular-compiler';
+import type { CallExpression, Node, SourceCode } from 'eslint';
 import {
   ImplicitReceiver,
   PropertyRead,
@@ -10,12 +10,11 @@ import {
 } from '../utils/create-eslint-rule';
 
 type Options = [];
-export type MessageIds = 'noAny' | 'suggestRemoveAny';
-export const RULE_NAME = 'no-any';
+type MessageIds = 'noAny' | 'suggestRemoveAny';
 const ANY_TYPE_CAST_FUNCTION_NAME = '$any';
 
 export default createESLintRule<Options, MessageIds>({
-  name: RULE_NAME,
+  name: 'no-any',
   meta: {
     type: 'suggestion',
     docs: {
@@ -35,38 +34,31 @@ export default createESLintRule<Options, MessageIds>({
     const sourceCode = context.getSourceCode();
 
     return {
-      [`Call[receiver.name="${ANY_TYPE_CAST_FUNCTION_NAME}"]`]({
-        receiver,
-        sourceSpan: { end, start },
-      }: Call) {
-        if (!(receiver instanceof PropertyRead)) {
-          return;
-        }
+      'CallExpression[callee.name="${ANY_TYPE_CAST_FUNCTION_NAME}"]': (
+        node: CallExpression
+      ) => {
+        const receiver = (node.object as Node & {
+          type: 'MemberExpression';
+        })?.property;
+
         if (
-          !(
-            // this.$any() is also valid usage of the native Angular $any()
-            (
-              receiver.receiver instanceof ThisReceiver ||
-              receiver.receiver instanceof ImplicitReceiver
-            )
-          )
+          !(receiver instanceof PropertyRead) ||
+          !(receiver.receiver instanceof ThisReceiver) ||
+          !(receiver.receiver instanceof ImplicitReceiver)
         ) {
           return;
         }
+
         const nameSpan = receiver.nameSpan;
         context.report({
+          node,
           messageId: 'noAny',
-          loc: {
-            start: sourceCode.getLocFromIndex(start),
-            end: sourceCode.getLocFromIndex(end),
-          },
+          loc: sourceCode.getLocFromIndex(nameSpan.start),
           suggest: [
             {
               messageId: 'suggestRemoveAny',
-              fix: (fixer) => [
+              fix: (fixer) =>
                 fixer.removeRange([nameSpan.start, nameSpan.end + 1]),
-                fixer.removeRange([end - 1, end]),
-              ],
             },
           ],
         });
