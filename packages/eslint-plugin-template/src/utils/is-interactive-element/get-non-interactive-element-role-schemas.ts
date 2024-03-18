1abe361ff03549f2a9e1b20aa6902a66c4bc7302
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:09 -0300

    improve

--- packages/eslint-plugin-template/src/utils/is-interactive-element/get-non-interactive-element-role-schemas.ts
+++ packages/eslint-plugin-template/src/utils/is-interactive-element/get-non-interactive-element-role-schemas.ts
@@ -1,46 +1,22 @@
-import type { ARIARoleDefintionKey, ARIARoleRelationConcept } from 'aria-query';
-import { elementRoles, roles } from 'aria-query';
+import type { ARIARoleDefinitionKey, ARIARoleRelationConcept } from "aria-query";
+import { elementRoles, roles } from "aria-query";
 
 let nonInteractiveElementRoleSchemas: ARIARoleRelationConcept[] | null = null;
 
-// This function follows the lazy initialization pattern.
-// Since this is a top-level module (it will be included via `require`), we do not need to
-// initialize the `nonInteractiveElementRoleSchemas` until the function is called
-// for the first time, so we will not take up the memory.
 export function getNonInteractiveElementRoleSchemas(): ARIARoleRelationConcept[] {
   if (nonInteractiveElementRoleSchemas === null) {
-    const roleKeys = [...roles.keys()];
-    const elementRoleEntries = [...elementRoles];
+    const nonInteractiveRoles = new Set<ARIARoleDefinitionKey>([
+      ...roles.keys()].filter((name) => {
+      const role = roles.get(name);
+      return role &&
+        !role.abstract &&
+        name !== 'toolbar' &&
+        !['widget', 'landmark', 'range'].some((superClass) => role.superClass.includes(superClass));
+    }}).concat('progressbar'));
 
-    const nonInteractiveRoles = new Set<ARIARoleDefintionKey>(
-      roleKeys
-        .filter((name) => {
-          const role = roles.get(name);
-          return (
-            role &&
-            !role.abstract &&
-            // 'toolbar' does not descend from widget, but it does support
-            // aria-activedescendant, thus in practice we treat it as a widget.
-            name !== 'toolbar' &&
-            !role.superClass.some((classes) => classes.includes('widget'))
-          );
-        })
-        .concat(
-          // The `progressbar` is descended from `widget`, but in practice, its
-          // value is always `readonly`, so we treat it as a non-interactive role.
-          'progressbar',
-        ),
+    nonInteractiveElementRoleSchemas = elementRoles.flatMap(([elementSchema, roleSet]) =>
+      roleSet.every(role => nonInteractiveRoles.has(role)) ? elementSchema : []
     );
-
-    nonInteractiveElementRoleSchemas = elementRoleEntries.reduce<
-      ARIARoleRelationConcept[]
-    >((accumulator, [elementSchema, roleSet]) => {
-      return accumulator.concat(
-        [...roleSet].every((role) => nonInteractiveRoles.has(role))
-          ? elementSchema
-          : [],
-      );
-    }, []);
   }
 
   return nonInteractiveElementRoleSchemas;
