e5e8d7871d75c5c329241ac218f9603420f4f826
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:08 -0300

    improve

--- packages/eslint-plugin-template/tests/rules/accessibility-table-scope/cases.ts
+++ packages/eslint-plugin-template/tests/rules/accessibility-table-scope/cases.ts
@@ -1,42 +1,39 @@
 import { convertAnnotatedSourceToFailureCase } from '@angular-eslint/utils';
 import type { MessageIds } from '../../../src/rules/accessibility-table-scope';
 
+type TestCase = [string, string?];
+
 const messageId: MessageIds = 'accessibilityTableScope';
 
-export const valid = [
-  '<th></th>',
-  '<th scope="col"></th>',
-  `<th [scope]="'col'"></th>`,
-  '<th [attr.scope]="scope"></th>',
-  '<div Scope="col"></div>',
-  '<button [appscope]="col"></button>',
-  '<app-table scope></app-table>',
-  '<app-row [scope]="row"></app-row>',
+const valid: TestCase[] = [
+  ['<th></th>'],
+  ['<th scope="col"></th>'],
+  ['<th [scope]="\'col\'"></th>'],
+  ['<th [attr.scope]="scope"></th>'],
+  ['<div scope="col"></div>'],
+  ['<button [appscope]="col"></button>'],
+  ['<app-table scope></app-table>'],
+  ['<app-row [scope]="row"></app-row>'],
 ];
 
-export const invalid = [
-  convertAnnotatedSourceToFailureCase({
-    description: 'should fail if `scope` attribute is not on `th` element',
-    annotatedSource: `
-        {{ test }}<div scope></div>
-                       ~~~~~
-      `,
-    messageId,
-    annotatedOutput: `
-        {{ test }}<div></div>
-                       
-      `,
-  }),
-  convertAnnotatedSourceToFailureCase({
-    description: 'should fail if `scope` input is not on `th` element',
-    annotatedSource: `
-        <div [attr.scope]="scope"></div><p></p>
-             ~~~~~~~~~~~~~~~~~~~~
-      `,
-    messageId,
-    annotatedOutput: `
-        <div></div><p></p>
-             
-      `,
-  }),
+const invalid: TestCase[] = [
+  [
+    '{{ test }}<div scope></div>',
+    'should fail if `scope` attribute is not on `th` element',
+    '{{ test }}<div></div>',
+  ],
+  [
+    '<div [attr.scope]="scope"></div><p></p>',
+    'should fail if `scope` input is not on `th` element',
+    '<div></div><p></p>',
+  ],
 ];
+
+invalid.forEach(([input, description, output]) => {
+  convertAnnotatedSourceToFailureCase({
+    description,
+    annotatedSource: `{{ test }}${input}`,
+    messageId,
+    annotatedOutput: `{{ test }}${output}`,
+  });
+});
