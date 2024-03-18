ade37a1e6a35ee119a09ceae667755b8d79928d3
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:32 -0300

    improve

--- packages/integration-tests/fixtures/v1123-multi-project-manual-config/projects/horse-plants/src/polyfills.ts
+++ packages/integration-tests/fixtures/v1123-multi-project-manual-config/projects/horse-plants/src/polyfills.ts
@@ -2,20 +2,43 @@
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
  *
- * This file is divided into 2 sections:
- *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
- *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
+ * This file is divided into 3 sections:
+ *   1. Browser polyfills for old browsers. These are applied before loading ZoneJS.
+ *   2. Browser polyfills for evergreen browsers. These are applied before loading ZoneJS and are
+ *      sorted by browsers.
+ *   3. Application imports. Files imported after ZoneJS that should be loaded before your main
  *      file.
  *
  * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
  * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
  * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
  *
+ * For older browsers, such as IE11, you may need to add additional polyfills. The following
+ * polyfills are included for older browsers:
+ * - es6-shim for ES6 class, symbol, arrow functions, etc.
+ * - es6-promise for Promise
+ * - reflect-metadata for decorators
+ * - web-animations-js for AnimationBuilder
+ * - classlist.js for NgClass support on SVG elements
+ *
  * Learn more in https://angular.io/guide/browser-support
  */
 
 /***************************************************************************************************
- * BROWSER POLYFILLS
+ * BROWSER POLYFILLS FOR OLD BROWSERS
+ */
+
+// ES6+ shims for old browsers
+import 'es6-shim';
+
+// Fallback for browsers that do not support Promise
+import 'es6-promise/auto';
+
+// For decorators
+import 'reflect-metadata';
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS FOR EVERGREEN BROWSERS
  */
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
@@ -53,10 +76,9 @@
  */
 
 /***************************************************************************************************
- * Zone JS is required by default for Angular itself.
+ * ZONE JS
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
