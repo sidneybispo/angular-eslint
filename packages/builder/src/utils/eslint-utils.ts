c4b7b09da5d5f068c4c811a698dab85fb24e0d38
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:11 -0300

    improve

--- packages/builder/src/utils/eslint-utils.ts
+++ packages/builder/src/utils/eslint-utils.ts
@@ -1,50 +1,42 @@
-import type * as ESLintLibrary from 'eslint';
+import * as ESLintLibrary from 'eslint';
 import { join } from 'path';
-import type { Schema } from '../schema';
+import { readFileSync } from 'fs';
 
 export async function loadESLint(): Promise<typeof ESLintLibrary> {
   let eslint;
   try {
+    // Use dynamic import here to avoid importing ESLint unnecessarily
     eslint = await import('eslint');
     return eslint;
-  } catch {
-    throw new Error('Unable to find ESLint. Ensure ESLint is installed.');
+  } catch (error) {
+    throw new Error('Unable to find ESLint. Ensure ESLint is installed: ' + error.message);
   }
 }
 
 export async function lint(
   workspaceRoot: string,
   eslintConfigPath: string | undefined,
-  options: Schema,
+  options: ESLintLibrary.ESLint.Options & { lintFilePatterns: string[] },
 ): Promise<ESLintLibrary.ESLint.LintResult[]> {
   const projectESLint = await loadESLint();
 
+  const eslintConfig = eslintConfigPath
+    ? JSON.parse(readFileSync(eslintConfigPath, 'utf-8'))
+    : undefined;
+
   const eslint = new projectESLint.ESLint({
-    /**
-     * If "noEslintrc" is set to `true` (and therefore here "useEslintrc" will be `false`), then ESLint will not
-     * merge the provided config with others it finds automatically.
-     */
-    useEslintrc: !options.noEslintrc,
-    overrideConfigFile: eslintConfigPath,
-    ignorePath: options.ignorePath || undefined,
-    fix: !!options.fix,
-    cache: !!options.cache,
-    cacheLocation: options.cacheLocation || undefined,
-    cacheStrategy: options.cacheStrategy || undefined,
-    resolvePluginsRelativeTo: options.resolvePluginsRelativeTo || undefined,
-    rulePaths: options.rulesdir || [],
-    /**
-     * Default is `true` and if not overridden the eslint.lintFiles() method will throw an error
-     * when no target files are found.
-     *
-     * We don't want ESLint to throw an error if a user has only just created
-     * a project and therefore doesn't necessarily have matching files, for example.
-     */
+    ...(eslintConfig && { overrideConfigFile: eslintConfig }),
+    ignorePath: options.ignorePath,
+    fix: options.fix,
+    cache: options.cache,
+    cacheLocation: options.cacheLocation,
+    cacheStrategy: options.cacheStrategy,
+    resolvePluginsRelativeTo: options.resolvePluginsRelativeTo,
+    rulePaths: options.rulePaths,
     errorOnUnmatchedPattern: false,
   });
 
   return await eslint.lintFiles(
-    // lintFilePatterns are defined relative to the root of the Angular-CLI workspace
     options.lintFilePatterns.map((p) => join(workspaceRoot, p)),
   );
 }
