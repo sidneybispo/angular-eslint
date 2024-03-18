efa0fefe5c45ac56e03ea6a517d984f5380ee800
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:10 -0300

    improve

--- packages/eslint-plugin-template/src/utils/is-child-node-of.ts
+++ packages/eslint-plugin-template/src/utils/is-child-node-of.ts
@@ -1,16 +1,13 @@
-import { TmplAstElement } from '@angular-eslint/bundled-angular-compiler';
+import { TmplAstElement, TmplAstChildNode } from '@angular-eslint/bundled-angular-compiler';
 
-export function isChildNodeOf(
-  ast: TmplAstElement,
-  childNodeName: string,
-): boolean {
-  function traverseChildNodes({ children }: TmplAstElement): boolean {
-    return children.some(
-      (child) =>
-        child instanceof TmplAstElement &&
-        (child.name === childNodeName || traverseChildNodes(child)),
+export function isChildNodeOf(ast: TmplAstElement, childNodeName: string): boolean {
+  function traverseChildNodes(nodes: TmplAstChildNode[]): boolean {
+    return nodes.some(
+      (node): node is TmplAstElement =>
+        node instanceof TmplAstElement &&
+        (node.name === childNodeName || traverseChildNodes(node.children)),
     );
   }
 
-  return traverseChildNodes(ast);
+  return traverseChildNodes(ast.children);
 }
