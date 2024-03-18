05c66bfe189a5725e99be977c9206d0d73909129
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:05:02 -0300

    improve

--- tools/scripts/postinstall.sh
+++ tools/scripts/postinstall.sh
@@ -1,14 +1,20 @@
-# In certain circumstances we want to skip the below the steps and it may not always
+#!/bin/bash
+
+# In certain circumstances we want to skip the below steps and it may not always
 # be possible to use --ignore-scripts (e.g. if another tool is what is invoking the
 # install command, such as when nx migrate runs). We therefore use and env var for this.
 
-if [ -n "$SKIP_POSTINSTALL" ]; then
-  echo "Skipping postinstall script..."
-  exit 0
+if [[ -n "$SKIP_POSTINSTALL" ]]; then
+    echo "Skipping postinstall script..."
+    exit 0
 fi
 
 # Build all the packages ready for use
-yarn build
+yarn build || { echo "Error: yarn build failed" && exit 1; }
 
 # Check for a clean workspace after install and build
-yarn check-clean-workspace-after-install
+if ! yarn check-clean-workspace-after-install; then
+    echo "Error: yarn check-clean-workspace-after-install failed"
+    exit 1
+fi
+
