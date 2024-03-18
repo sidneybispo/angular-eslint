3f9d287ea1d6d9611b53bd7266b043bb6cfc361e
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:15 -0300

    improve

--- packages/eslint-plugin/tests/rules/contextual-lifecycle/spec.ts
+++ packages/eslint-plugin/tests/rules/contextual-lifecycle/spec.ts
@@ -1,12 +1,70 @@
 import { RuleTester } from '@angular-eslint/utils';
+import { parseSource } from '@eslint-plugins/angular/utils';
 import rule, { RULE_NAME } from '../../../src/rules/contextual-lifecycle';
-import { invalid, valid } from './cases';
+import {
+  createRuleTesterTest,
+  LifecycleMethods,
+} from '../../test-utils/rule-test-utils';
 
 const ruleTester = new RuleTester({
-  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
 });
 
-ruleTester.run(RULE_NAME, rule, {
-  valid,
-  invalid,
-});
+const lifecycleMethods: LifecycleMethods = ['ngOnInit', 'ngOnDestroy'];
+
+createRuleTesterTest(ruleTester, rule, {
+  valid: [
+    {
+      name: 'should pass when using lifecycle methods correctly',
+      code: valid,
+    },
+  ],
+  invalid: [
+    {
+      name: 'should fail when using ngOnInit in the constructor',
+      code: invalid.constructor,
+      errors: [
+        {
+          messageId: 'disallowInConstructor',
+          data: {
+            methodName: lifecycleMethods[0],
+          },
+        },
+      ],
+    },
+    {
+      name: 'should fail when using ngOnInit in the wrong place',
+      code: invalid.wrongPlace,
+      errors: [
+        {
+          messageId: 'disallowOutsideComponent',
+          data: {
+            methodName: lifecycleMethods[0],
+          },
+        },
+      ],
+    },
+    {
+      name: 'should fail when using ngOnInit without constructor',
+      code: invalid.noConstructor,
+      errors: [
+        {
+          messageId: 'requireConstructor',
+        },
+      ],
+    },
+    {
+      name: 'should fail when using ngOnInit with wrong parameters',
+      code: invalid.wrongParameters,
+      errors: [
+        {
+          messageId: 'invalidParameters',
+          data: {
+            methodName: lifecycleMethods[0],
+            expectedParameters: '(void)',
+            receivedParameters: '(number)',
+          },
+        },
