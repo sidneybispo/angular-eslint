f836903a0d667de305a2413d9783bee0e9db3505
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:16 -0300

    improve

--- packages/eslint-plugin/tests/rules/directive-class-suffix/spec.ts
+++ packages/eslint-plugin/tests/rules/directive-class-suffix/spec.ts
@@ -1,12 +1,63 @@
 import { RuleTester } from '@angular-eslint/utils';
-import rule, { RULE_NAME } from '../../../src/rules/directive-class-suffix';
-import { invalid, valid } from './cases';
+import { FormDirective } from './form.directive';
+import { TsConfigRuleTester } from '@angular-eslint/utils/testing';
+import rule, { RULE_NAME } from '../../src/rules/directive-class-suffix';
 
-const ruleTester = new RuleTester({
-  parser: '@typescript-eslint/parser',
-});
+describe(RULE_NAME, () => {
+  let ruleTester: TsConfigRuleTester;
 
-ruleTester.run(RULE_NAME, rule, {
-  valid,
-  invalid,
+  beforeEach(() => {
+    ruleTester = new TsConfigRuleTester({
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    });
+  });
+
+  it('reports violations', () => {
+    ruleTester.run(RULE_NAME, rule, {
+      invalid: [
+        {
+          code: `
+            @Directive({
+              selector: '[appFoo]'
+            })
+            export class FooDirective {
+            }
+          `,
+          errors: [{ messageId: 'expectedDirectiveClassSuffix', data: { suffix: 'Directive' } }],
+        },
+      ],
+      valid: [
+        {
+          code: `
+            @Directive({
+              selector: '[appBar]'
+            })
+            export class BarDirective {
+            }
+          `,
+        },
+        {
+          code: `
+            @Component({
+              selector: 'app-foo',
+              template: \`\`
+            })
+            export class FooComponent {
+            }
+          `,
+        },
+        {
+          code: `
+            @Directive({
+              selector: '[appFoo]'
+            })
+            export class FooFormDirective extends FormDirective {
+            }
+          `,
+        },
+      ],
+    });
+  });
 });
