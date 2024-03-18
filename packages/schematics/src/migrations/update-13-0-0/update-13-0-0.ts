16474f48a5c17cf6f5949d8767e1019b91fdffb9
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:51 -0300

    improve

--- packages/schematics/src/migrations/update-13-0-0/update-13-0-0.ts
+++ packages/schematics/src/migrations/update-13-0-0/update-13-0-0.ts
@@ -1,34 +1,21 @@
-import type { Rule } from '@angular-devkit/schematics';
+import { Rule, SchematicsException } from '@angular-devkit/schematics';
 import { chain } from '@angular-devkit/schematics';
 import { updateDependencies } from '../utils/dependencies';
 
-const updatedTypeScriptESLintVersion = '5.3.0';
-const updatedESLintVersion = '8.2.0';
-const updatedESLintPluginImportVersion = '2.25.2';
+const requiredVersions = {
+  '@typescript-eslint/eslint-plugin': '5.3.0',
+  '@typescript-eslint/experimental-utils': '5.3.0',
+  '@typescript-eslint/parser': '5.3.0',
+  eslint: '^8.2.0',
+  'eslint-plugin-import': '2.25.2',
+};
 
 export default function migration(): Rule {
-  return chain([
-    updateDependencies([
-      {
-        packageName: '@typescript-eslint/eslint-plugin',
-        version: updatedTypeScriptESLintVersion,
-      },
-      {
-        packageName: '@typescript-eslint/experimental-utils',
-        version: updatedTypeScriptESLintVersion,
-      },
-      {
-        packageName: '@typescript-eslint/parser',
-        version: updatedTypeScriptESLintVersion,
-      },
-      {
-        packageName: 'eslint',
-        version: `^${updatedESLintVersion}`,
-      },
-      {
-        packageName: 'eslint-plugin-import',
-        version: updatedESLintPluginImportVersion,
-      },
-    ]),
-  ]);
+  return chain(() => {
+    try {
+      return updateDependencies(requiredVersions);
+    } catch (error) {
+      throw new SchematicsException(error.message);
+    }
+  });
 }
