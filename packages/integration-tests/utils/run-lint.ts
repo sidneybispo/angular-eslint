fcdabab2285aefadebd035fa5c0443050e725c51
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:51 -0300

    improve

--- packages/integration-tests/utils/run-lint.ts
+++ packages/integration-tests/utils/run-lint.ts
@@ -1,47 +1,42 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import execa from 'execa';
 import path from 'path';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const stripAnsi = require('strip-ansi');
+import stripAnsi from 'strip-ansi';
 
 const FIXTURES_DIR = path.join(__dirname, '../fixtures/');
 
-function normalizeOutput(value: string): string {
-  const ansiRemoved: string = stripAnsi(value);
-  return (
-    ansiRemoved
-      .replace(
-        new RegExp(`^${FIXTURES_DIR.replace(/\\/g, '\\\\')}(.*?)$`, 'gm'),
-        (_, c1) => `__ROOT__/${c1.replace(/\\/g, '/')}`,
-      )
-      /**
-       * This one is ridiculous...
-       *
-       * No matter what I try (including the use of `strip-ansi`), I do not seem to be
-       * able to stop the inclusion of this extra `m` any other way (it is presumably
-       * coming from an ANSI code becuase of its initial appearance in CI as [27m).
-       * It happens locally and CI... but only exactly once.
-       */
-      .replace('mmax-len', 'max-len')
+type NormalizeOutputFn = (value: string) => string;
+
+const normalizeOutput: NormalizeOutputFn = (value) => {
+  const ansiRemoved = stripAnsi(value);
+  return ansiRemoved.replace(
+    new RegExp(`^${FIXTURES_DIR.replace(/\\/g, '\\\\')}(.*?)$`, 'gm'),
+    (_, c1) => `__ROOT__/${c1.replace(/\\/g, '/')}`,
   );
-}
+};
 
-export async function runLint(directory: string): Promise<string | undefined> {
+type RunLintFn = (directory: string) => Promise<string | undefined>;
+
+const runLint: RunLintFn = async (directory) => {
   try {
-    const subprocess = execa('npx', ['ng', 'lint'], {
-      cwd: path.join(FIXTURES_DIR, directory),
-    });
+    const command = 'npx';
+    const args = ['ng', 'lint'];
+    const cwd = path.join(FIXTURES_DIR, directory);
 
-    /* eslint-disable @typescript-eslint/no-non-null-assertion */
-    subprocess.stdout!.pipe(process.stdout);
-    subprocess.stderr!.pipe(process.stderr);
-    /* eslint-enable @typescript-eslint/no-non-null-assertion */
+    const subprocess = execa(command, args, { cwd });
+
+    subprocess.stdout?.pipe(process.stdout);
+    subprocess.stderr?.pipe(process.stderr);
 
     const { stdout } = await subprocess;
 
     return normalizeOutput(stdout);
-  } catch (error: any) {
-    return normalizeOutput(error.stdout || error);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.error(`Command '${error.command}' not found.`);
+    } else {
+      console.error(error);
+    }
   }
-}
+};
+
+export default runLint;
