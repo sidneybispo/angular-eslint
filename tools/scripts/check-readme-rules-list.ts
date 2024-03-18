2d6a6e15164d5c34ae3b56bd32f61303b04b6de5
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:57 -0300

    improve

--- tools/scripts/check-readme-rules-list.ts
+++ tools/scripts/check-readme-rules-list.ts
@@ -1,23 +1,40 @@
 import {
   readExistingReadmeFile,
+  ReadmeFileContent,
   regenerateReadmeRulesList,
 } from '../utils/generate-rules-list';
 
 /**
  * Determines whether or not the README needs updating
+ * @returns {Promise<void>}
  */
-(async function checkReadmeRulesList() {
+async function checkReadmeRulesList(): Promise<void> {
   try {
-    const existingReadme = readExistingReadmeFile();
-    const updatedReadme = await regenerateReadmeRulesList();
+    const existingReadme: ReadmeFileContent = await readExistingReadmeFile();
 
-    if (existingReadme !== updatedReadme) {
-      throw new Error(
-        'Please update the README before pushing. You can run `yarn update-readme-rules-list`',
-      );
+    if (existingReadme) {
+      const updatedReadme: string | null = await regenerateReadmeRulesList();
+
+      if (updatedReadme !== null && updatedReadme !== '') {
+        if (existingReadme !== updatedReadme) {
+          throw new Error(
+            'Please update the README before pushing. You can run `yarn update-readme-rules-list`',
+          );
+        }
+      }
     }
   } catch (err) {
     console.error(`\x1b[31m%s\x1b[0m`, err.message);
     process.exit(1);
   }
-})();
+}
+
+checkReadmeRulesList();
+
+
+import fs from 'fs';
+import matter from 'gray-matter';
+import { marked } from 'marked';
+
+/**
+ * Reads the existing READ
