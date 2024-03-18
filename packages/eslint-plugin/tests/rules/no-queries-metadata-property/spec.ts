56c13d50a3bbff9306ce80288e700b5705fea4be
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:24 -0300

    improve

--- packages/eslint-plugin/tests/rules/no-queries-metadata-property/spec.ts
+++ packages/eslint-plugin/tests/rules/no-queries-metadata-property/spec.ts
@@ -1,15 +1,45 @@
 import { RuleTester } from '@angular-eslint/utils';
-import rule, {
-  RULE_NAME,
-} from '../../../src/rules/no-queries-metadata-property';
+import { parseSource } from '@angular-eslint/template-parser';
+import rule, { RULE_NAME } from '../../../src/rules/no-queries-metadata-property';
 
 import { invalid, valid } from './cases';
 
 const ruleTester = new RuleTester({
-  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
 });
 
 ruleTester.run(RULE_NAME, rule, {
-  valid,
-  invalid,
+  valid: [
+    {
+      code: valid,
+    },
+    {
+      code: `
+        @Component({
+          selector: 'app-example',
+          template: \`
+            <div [innerHtml]="exampleHtml | safe: 'html'"></div>
+          \`,
+        })
+        class ExampleComponent {
+          exampleHtml = '<p>Example HTML</p>';
+        }
+      `,
+    },
+  ].map((testCase) => ({
+    ...testCase,
+    template: parseSource(testCase.code).templates[0],
+  })),
+  invalid: [
+    {
+      code: invalid,
+      errors: [{ messageId: 'noQueriesMetadataProperty' }],
+    },
+  ].map((testCase) => ({
+    ...testCase,
+    template: parseSource(testCase.code).templates[0],
+  })),
 });
