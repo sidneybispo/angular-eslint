d4b5172a1471c8eae5f13a69ad6be55c8c37379c
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:05:04 -0300

    improve

--- packages/schematics/src/ng-add/index.ts
+++ packages/schematics/src/ng-add/index.ts
@@ -1,72 +1,110 @@
 import type { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { chain, schematic } from '@angular-devkit/schematics';
-import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import {
   getTargetsConfigFromProject,
   readJsonInTree,
   sortObjectByKeys,
   updateJsonInTree,
 } from '../utils';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageJSON = require('../../package.json');
 
-function addAngularESLintPackages() {
-  return (host: Tree, context: SchematicContext) => {
-    if (!host.exists('package.json')) {
-      throw new Error(
-        'Could not find a `package.json` file at the root of your workspace',
-      );
-    }
+interface PackageJson {
+  devDependencies?: {
+    [key: string]: string;
+  };
+  scripts?: {
+    [key: string]: string;
+  };
+}
 
-    if (host.exists('tsconfig.base.json')) {
-      throw new Error(
-        '\nError: Angular CLI v10.1.0 and later (and no `tsconfig.base.json`) is required in order to run this schematic. Please update your workspace and try again.\n',
-      );
-    }
-
-    const projectPackageJSON = (host.read('package.json') as Buffer).toString(
-      'utf-8',
+function addAngularESLintPackages(host: Tree): void {
+  if (!host.exists('package.json')) {
+    throw new Error(
+      'Could not find a `package.json` file at the root of your workspace',
     );
-    const json = JSON.parse(projectPackageJSON);
-    json.devDependencies = json.devDependencies || {};
-    json.devDependencies[
-      'eslint'
-    ] = `^${packageJSON.devDependencies['eslint']}`;
-    json.scripts = json.scripts || {};
-    json.scripts['lint'] = json.scripts['lint'] || 'ng lint';
+  }
 
-    /**
-     * @angular-eslint packages
-     */
-    json.devDependencies['@angular-eslint/builder'] = packageJSON.version;
-    json.devDependencies['@angular-eslint/eslint-plugin'] = packageJSON.version;
-    json.devDependencies['@angular-eslint/eslint-plugin-template'] =
-      packageJSON.version;
-    /**
-     * It seems in certain versions of Angular CLI `ng add` will automatically add the
-     * @angular-eslint/schematics package to the dependencies section, so clean that up
-     * at this point
-     */
-    if (json.dependencies?.['@angular-eslint/schematics']) {
-      delete json.dependencies['@angular-eslint/schematics'];
-    }
-    json.devDependencies['@angular-eslint/schematics'] = packageJSON.version;
-    json.devDependencies['@angular-eslint/template-parser'] =
-      packageJSON.version;
+  if (host.exists('tsconfig.base.json')) {
+    throw new Error(
+      '\nError: Angular CLI v10.1.0 and later (and no `tsconfig.base.json`) is required in order to run this schematic. Please update your workspace and try again.\n',
+    );
+  }
 
-    /**
-     * @typescript-eslint packages
-     */
-    const typescriptESLintVersion =
-      packageJSON.devDependencies['@typescript-eslint/experimental-utils'];
-    json.devDependencies['@typescript-eslint/eslint-plugin'] =
-      typescriptESLintVersion;
-    json.devDependencies['@typescript-eslint/parser'] = typescriptESLintVersion;
+  const projectPackageJSON = (host.read('package.json') as Buffer).toString(
+    'utf-8',
+  );
+  const json: PackageJson = JSON.parse(projectPackageJSON);
 
-    json.devDependencies = sortObjectByKeys(json.devDependencies);
-    host.overwrite('package.json', JSON.stringify(json, null, 2));
+  json.devDependencies = json.devDependencies || {};
+  json.devDependencies['eslint'] = `^${packageJSON.devDependencies['eslint']}`;
+  json.scripts = json.scripts || {};
+  json.scripts['lint'] = json.scripts['lint'] || 'ng lint';
 
+  json.devDependencies['@angular-eslint/builder'] = packageJSON.version;
+  json.devDependencies['@angular-eslint/eslint-plugin'] = packageJSON.version;
+  json.devDependencies['@angular-eslint/eslint-plugin-template'] =
+    packageJSON.version;
+  if (json.dependencies?.['@angular-eslint/schematics']) {
+    delete json.dependencies['@angular-eslint/schematics'];
+  }
+  json.devDependencies['@angular-eslint/schematics'] = packageJSON.version;
+  json.devDependencies['@angular-eslint/template-parser'] =
+    packageJSON.version;
+
+  const typescriptESLintVersion =
+    packageJSON.devDependencies['@typescript-eslint/experimental-utils'];
+  json.devDependencies['@typescript-eslint/eslint-plugin'] =
+    typescriptESLintVersion;
+  json.devDependencies['@typescript-eslint/parser'] = typescriptESLintVersion;
+
+  json.devDependencies = sortObjectByKeys(json.devDependencies);
+  host.overwrite('package.json', JSON.stringify(json, null, 2));
+}
+
+function applyESLintConfigIfSingleProjectWithNoExistingTSLint(host: Tree): void {
+  const angularJson = readJsonInTree(host, 'angular.json');
+  if (!angularJson || !angularJson.projects) {
+    return;
+  }
+  const projectNames = Object.keys(angularJson.projects);
+  if (projectNames.length !== 1) {
+    return;
+  }
+
+  const singleProject = angularJson.projects[projectNames[0]];
+  const targetsConfig = getTargetsConfigFromProject(singleProject);
+  if (!targetsConfig) {
+    return;
+  }
+
+  if (targetsConfig.lint) {
+    return;
+  }
+
+  addEslintToProject(host);
+
+  updateJsonInTree('angular.json', (json) => {
+    json.cli = json.cli || {};
+    json.cli.defaultCollection = '@angular-eslint/schematics';
+    return json;
+  });
+}
+
+function addEslintToProject(host: Tree): void {
+  const projectName = Object.keys(
+    readJsonInTree(host, 'angular.json').projects,
+  )[0];
+  schematic('add-eslint', {
+    project: projectName,
+  })(host, {} as SchematicContext);
+}
+
+export default function (): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    addAngularESLintPackages(host);
+    applyESLintConfigIfSingleProjectWithNoExistingTSLint(host);
     context.addTask(new NodePackageInstallTask());
 
     context.logger.info(`
@@ -76,55 +114,5 @@ Please see https://github.com/angular-eslint/angular-eslint for how to add ESLin
 `);
 
     return host;
-  };
-}
-
-function applyESLintConfigIfSingleProjectWithNoExistingTSLint() {
-  return (host: Tree, context: SchematicContext) => {
-    const angularJson = readJsonInTree(host, 'angular.json');
-    if (!angularJson || !angularJson.projects) {
-      return;
-    }
-    // Anything other than a single project, finish here as there is nothing more we can do automatically
-    const projectNames = Object.keys(angularJson.projects);
-    if (projectNames.length !== 1) {
-      return;
-    }
-
-    const singleProject = angularJson.projects[projectNames[0]];
-    const targetsConfig = getTargetsConfigFromProject(singleProject);
-    // Only possible if malformed, safer to finish here
-    if (!targetsConfig) {
-      return;
-    }
-
-    // The project already has a lint builder setup, finish here as there is nothing more we can do automatically
-    if (targetsConfig.lint) {
-      return;
-    }
-
-    context.logger.info(`
-We detected that you have a single project in your workspace and no existing linter wired up, so we are configuring ESLint for you automatically.
-
-Please see https://github.com/angular-eslint/angular-eslint for more information.
-`);
-
-    return chain([
-      schematic('add-eslint-to-project', {}),
-      updateJsonInTree('angular.json', (json) => {
-        json.cli = json.cli || {};
-        json.cli.defaultCollection = '@angular-eslint/schematics';
-        return json;
-      }),
-    ]);
-  };
-}
-
-export default function (): Rule {
-  return (host: Tree, context: SchematicContext) => {
-    return chain([
-      addAngularESLintPackages(),
-      applyESLintConfigIfSingleProjectWithNoExistingTSLint(),
-    ])(host, context);
   };
 }
