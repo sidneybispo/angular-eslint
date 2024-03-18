984de5db767b4e8c18f649980b88a2f1c6fa88f8
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:05:05 -0300

    improve

--- packages/utils/src/eslint-plugin/selectors.ts
+++ packages/utils/src/eslint-plugin/selectors.ts
@@ -22,43 +22,35 @@ export const OUTPUT_DECORATOR = 'Decorator[expression.callee.name="Output"]';
 
 export const LITERAL_OR_TEMPLATE_ELEMENT = ':matches(Literal, TemplateElement)';
 
-export function metadataProperty(key: RegExp): string;
-export function metadataProperty<TKey extends string>(
-  key: TKey,
-): `Property:matches([key.name=${TKey}][computed=false], [key.value=${TKey}], [key.quasis.0.value.raw=${TKey}])`;
-export function metadataProperty(key: RegExp | string): string {
-  return `Property:matches([key.name=${key}][computed=false], [key.value=${key}], [key.quasis.0.value.raw=${key}])`;
+export class MetadataProperty {
+  constructor(private key: RegExp | string) {}
+
+  toString(): string {
+    return `Property:matches([key.name=${this.key}][computed=false], [key.value=${this.key}], [key.quasis.0.value.raw=${this.key}])`;
+  }
 }
 
-export function methodDefinition(key: RegExp): string;
-export function methodDefinition<TKey extends string>(
-  key: TKey,
-): `MethodDefinition:matches([key.name=${TKey}][computed=false], [key.value=${TKey}], [key.quasis.0.value.raw=${TKey}])`;
-export function methodDefinition(key: RegExp | string): string {
-  return `MethodDefinition:matches([key.name=${key}][computed=false], [key.value=${key}], [key.quasis.0.value.raw=${key}])`;
+export class MethodDefinition {
+  constructor(private key: RegExp | string) {}
+
+  toString(): string {
+    return `MethodDefinition:matches([key.name=${this.key}][computed=false], [key.value=${this.key}], [key.quasis.0.value.raw=${this.key}])`;
+  }
 }
 
-export const COMPONENT_SELECTOR_LITERAL = `${COMPONENT_CLASS_DECORATOR} ${metadataProperty(
-  'selector',
-)} ${LITERAL_OR_TEMPLATE_ELEMENT}`;
+export const COMPONENT_SELECTOR_LITERAL = `${COMPONENT_CLASS_DECORATOR} ${new MetadataProperty('selector')} ${LITERAL_OR_TEMPLATE_ELEMENT}`;
 
-export const DIRECTIVE_SELECTOR_LITERAL = `${DIRECTIVE_CLASS_DECORATOR} ${metadataProperty(
-  'selector',
-)} ${LITERAL_OR_TEMPLATE_ELEMENT}`;
+export const DIRECTIVE_SELECTOR_LITERAL = `${DIRECTIVE_CLASS_DECORATOR} ${new MetadataProperty('selector')} ${LITERAL_OR_TEMPLATE_ELEMENT}`;
 
 export const COMPONENT_OR_DIRECTIVE_SELECTOR_LITERAL = `:matches(${COMPONENT_SELECTOR_LITERAL}, ${DIRECTIVE_SELECTOR_LITERAL})`;
 
-export const INPUTS_METADATA_PROPERTY_LITERAL = `${COMPONENT_OR_DIRECTIVE_CLASS_DECORATOR} ${metadataProperty(
-  'inputs',
-)} > ArrayExpression ${LITERAL_OR_TEMPLATE_ELEMENT}`;
+export const INPUTS_METADATA_PROPERTY_LITERAL = `${COMPONENT_OR_DIRECTIVE_CLASS_DECORATOR} ${new MetadataProperty('inputs')} > ArrayExpression ${LITERAL_OR_TEMPLATE_ELEMENT}`;
 
 export const INPUT_ALIAS = `:matches(PropertyDefinition, MethodDefinition[kind='set']) ${INPUT_DECORATOR} ${LITERAL_OR_TEMPLATE_ELEMENT}`;
 
 export const INPUT_PROPERTY_OR_SETTER = `:matches(PropertyDefinition, MethodDefinition[kind='set'])[computed=false]:has(${INPUT_DECORATOR}) > :matches(Identifier, Literal)`;
 
-export const OUTPUTS_METADATA_PROPERTY_LITERAL = `${COMPONENT_OR_DIRECTIVE_CLASS_DECORATOR} ${metadataProperty(
-  'outputs',
-)} > ArrayExpression ${LITERAL_OR_TEMPLATE_ELEMENT}`;
+export const OUTPUTS_METADATA_PROPERTY_LITERAL = `${COMPONENT_OR_DIRECTIVE_CLASS_DECORATOR} ${new MetadataProperty('outputs')} > ArrayExpression ${LITERAL_OR_TEMPLATE_ELEMENT}`;
 
 export const OUTPUT_ALIAS = `:matches(PropertyDefinition, MethodDefinition[kind='get']) ${OUTPUT_DECORATOR} ${LITERAL_OR_TEMPLATE_ELEMENT}`;
 
