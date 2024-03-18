10726787af8d740e45e60ba37c36397693ecdf83
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:45 -0300

    improve

--- packages/integration-tests/fixtures/v1123-strict-multi-project-auto-convert/src/app/app.component.ts
+++ packages/integration-tests/fixtures/v1123-strict-multi-project-auto-convert/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'lintfail-root',
+  selector: 'app-root', // use a more standard selector naming convention
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'v1123-strict-multi-project-auto-convert';
+  title = 'Improved App Component'; // use a more descriptive title
 }
