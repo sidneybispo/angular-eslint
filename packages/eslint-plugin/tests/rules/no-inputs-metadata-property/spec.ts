02bb9373ec049bce2d799235ba76e72bf987d016
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:19 -0300

    improve

--- packages/eslint-plugin/tests/rules/no-inputs-metadata-property/spec.ts
+++ packages/eslint-plugin/tests/rules/no-inputs-metadata-property/spec.ts
@@ -1,15 +1,32 @@
 import { RuleTester } from '@angular-eslint/utils';
-import rule, {
-  RULE_NAME,
-} from '../../../src/rules/no-inputs-metadata-property';
+import { parseSource } from '@angular-eslint/template-parser';
+import rule, { RULE_NAME } from '../../../src/rules/no-inputs-metadata-property';
 
 import { invalid, valid } from './cases';
 
 const ruleTester = new RuleTester({
-  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
 });
 
 ruleTester.run(RULE_NAME, rule, {
-  valid,
-  invalid,
+  valid: [
+    {
+      code: valid,
+    },
+    {
+      template: `
+        <app-my-component [someInput]="someValue"></app-my-component>
+      `,
+      templateSyntax: parseSource(`
+        <app-my-component [someInput]="someValue"></app-my-component>
+      `),
+    },
+  ].filter(Boolean),
+  invalid: invalid.map(({ code, errors }) => ({
+    code,
+    errors: errors.map((error) => ({ ...error, messageId: error.messageId.replace('noInputsMetadataProperty', RULE_NAME) })),
+  })),
 });
