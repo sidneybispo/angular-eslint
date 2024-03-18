f2205d69b538c83ef6d5f31fe39dbd391078f9f9
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:52 -0300

    improve

--- packages/schematics/src/library/index.ts
+++ packages/schematics/src/library/index.ts
@@ -1,38 +1,63 @@
-import type { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-import { chain, externalSchematic } from '@angular-devkit/schematics';
-/**
- * We are able to use the full, unaltered Schema directly from @schematics/angular
- * The applicable json file is copied from node_modules as a prebuiid step to ensure
- * they stay in sync.
- */
-import type { Schema } from '@schematics/angular/library/schema';
+import {
+  chain,
+  externalSchematic,
+  MergeStrategy,
+  mergeWith,
+  Rule,
+  SchematicContext,
+  Tree,
+} from '@angular-devkit/schematics';
+import { Schema as LibrarySchema } from '@schematics/angular/library/schema';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import {
   addESLintTargetToProject,
   createESLintConfigForProject,
   removeTSLintJSONForProject,
 } from '../utils';
 
-function eslintRelatedChanges(options: Schema) {
-  /**
-   * The types coming from the @schematics/angular schema seem to be wrong, if name isn't
-   * provided the interactive CLI prompt will throw
-   */
-  const projectName = options.name as string;
-  return chain([
-    // Update the lint builder and config in angular.json
-    addESLintTargetToProject(projectName, 'lint'),
-    // Create the ESLint config file for the project
-    createESLintConfigForProject(projectName),
-    // Delete the TSLint config file for the project
-    removeTSLintJSONForProject(projectName),
-  ]);
-}
-
-export default function (options: Schema): Rule {
+function eslintRelatedChanges(projectName: string): Rule {
   return (host: Tree, context: SchematicContext) => {
     return chain([
-      externalSchematic('@schematics/angular', 'library', options),
-      eslintRelatedChanges(options),
+      addESLintTargetToProject(projectName, 'lint'),
+      createESLintConfigForProject(projectName),
+      removeTSLintJSONForProject(projectName),
     ])(host, context);
   };
 }
+
+export default function (options: LibrarySchema): Rule {
+  const projectName = options.name;
+
+  return (host: Tree, context: SchematicContext) => {
+    return externalSchematic('@schematics/angular', 'library', options)(host, context)
+      .pipe(
+        mergeWith(
+          eslintRelatedChanges(projectName),
+          MergeStrategy.Overwrite
+        )
+      );
+  };
+}
+
+// Add this function to install the required dev dependencies
+function installDependencies() {
+  return () => {
+    const tasks: NodePackageInstallTask[] = [
+      {
+        name: 'eslint',
+        url: 'https://www.npmjs.com/package/eslint',
+      },
+      {
+        name: 'eslint-plugin-angular',
+        url: 'https://www.npmjs.com/package/eslint-plugin-angular',
+      },
+      {
+        name: 'eslint-plugin-jasmine',
+        url: 'https://www.npmjs.com/package/eslint-plugin-jasmine',
+      },
+      {
+        name: 'eslint-plugin-jest',
+        url: 'https://www.npmjs.com/package/eslint-plugin-jest',
+      },
+      {
+        name: 'eslint-plugin
