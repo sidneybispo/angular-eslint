f82f4fd5577e93a0e9f5daf3757c7ff5539e8104
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:18 -0300

    improve

--- packages/eslint-plugin/tests/rules/no-input-prefix/spec.ts
+++ packages/eslint-plugin/tests/rules/no-input-prefix/spec.ts
@@ -1,12 +1,55 @@
 import { RuleTester } from '@angular-eslint/utils';
 import rule, { RULE_NAME } from '../../../src/rules/no-input-prefix';
-import { invalid, valid } from './cases';
+import { createValidTests, createInvalidTests } from '../rule-test-utils';
 
 const ruleTester = new RuleTester({
   parser: '@typescript-eslint/parser',
 });
 
-ruleTester.run(RULE_NAME, rule, {
-  valid,
-  invalid,
+const validTests = createValidTests({
+  ruleName: RULE_NAME,
+  casesPath: './cases/valid.ts',
 });
+
+const invalidTests = createInvalidTests({
+  ruleName: RULE_NAME,
+  casesPath: './cases/invalid.ts',
+});
+
+ruleTester.run(RULE_NAME, rule, {
+  valid: validTests,
+  invalid: invalidTests,
+});
+
+
+// rule-test-utils.ts
+
+export const createValidTests = ({ ruleName, casesPath }) => {
+  return {
+    valid: {
+      [casesPath]: {
+        code: require(casesPath).default,
+        errors: [],
+      },
+    },
+  };
+};
+
+export const createInvalidTests = ({ ruleName, casesPath }) => {
+  return {
+    invalid: {
+      [casesPath]: {
+        code: require(casesPath).default,
+        errors: require(casesPath).errors,
+      },
+    },
+  };
+};
+
+
+// cases/valid.ts
+
+export default {
+  testComponent: `
+    import { Component } from '@angular/core';
+
