cafa6a6babcdb251ca37f6031a889fb2024f0cc1
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:59 -0300

    improve

--- packages/schematics/tests/migrations/update-4-0-0/update-4-0-0.test.ts
+++ packages/schematics/tests/migrations/update-4-0-0/update-4-0-0.test.ts
@@ -3,51 +3,54 @@ import {
   UnitTestTree,
 } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
-import { Tree } from '@angular-devkit/schematics';
+import { Tree, readContent } from '@angular-devkit/schematics';
 
 const migrationSchematicRunner = new SchematicTestRunner(
   '@angular-eslint/schematics',
   path.join(__dirname, '../../../src/migrations.json'),
 );
 
-describe('update-4-0-0', () => {
+describe('update-4-0-0 migration', () => {
   let appTree: UnitTestTree;
-  beforeEach(() => {
+
+  beforeEach(async () => {
     appTree = new UnitTestTree(Tree.empty());
-    appTree.create(
-      'package.json',
-      JSON.stringify({
-        devDependencies: {
-          '@angular-eslint/builder': '3.0.1',
-          '@angular-eslint/eslint-plugin': '3.0.1',
-          '@angular-eslint/eslint-plugin-template': '3.0.1',
-          '@angular-eslint/template-parser': '3.0.1',
+
+    const packageJsonContent = `{
+      "devDependencies": {
+        "@angular-eslint/builder": "3.0.1",
+        "@angular-eslint/eslint-plugin": "3.0.1",
+        "@angular-eslint/eslint-plugin-template": "3.0.1",
+        "@angular-eslint/template-parser": "3.0.1"
+      }
+    }`;
+
+    appTree.create('package.json', packageJsonContent);
+
+    const angularJsonContent = `{
+      "$schema": "./node_modules/@angular/cli/lib/config/schema.json",
+      "version": 1,
+      "newProjectRoot": "projects",
+      "projects": {
+        "foo": {
+          "root": "projects/foo"
         },
-      }),
-    );
-    appTree.create(
-      'angular.json',
-      JSON.stringify({
-        $schema: './node_modules/@angular/cli/lib/config/schema.json',
-        version: 1,
-        newProjectRoot: 'projects',
-        projects: {
-          foo: {
-            root: 'projects/foo',
-          },
-          bar: {
-            root: 'projects/bar',
-          },
-        },
-      }),
-    );
+        "bar": {
+          "root": "projects/bar"
+        }
+      }
+    }`;
+
+    appTree.create('angular.json', angularJsonContent);
   });
 
   it('should update relevant @angular-eslint dependencies', async () => {
     const tree = await migrationSchematicRunner
       .runSchematicAsync('update-4-0-0', {}, appTree)
       .toPromise();
-    const packageJSON = JSON.parse(tree.readContent('/package.json'));
+
+    const packageJSONContent = readContent(tree, '/package.json');
+    const packageJSON = JSON.parse(packageJSONContent);
     expect(packageJSON).toMatchInlineSnapshot(`
       Object {
         "devDependencies": Object {
