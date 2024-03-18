a4ed094dde0954ee1c91e0715161acb70689ffd3
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:09 -0300

    improve

--- packages/eslint-plugin-template/tests/rules/mouse-events-have-key-events/spec.ts
+++ packages/eslint-plugin-template/tests/rules/mouse-events-have-key-events/spec.ts
@@ -1,15 +1,39 @@
 import { RuleTester } from '@angular-eslint/utils';
-import rule, {
-  RULE_NAME,
-} from '../../../src/rules/mouse-events-have-key-events';
-
-import { invalid, valid } from './cases';
+import rule from '../../../src/rules/mouse-events-have-key-events';
 
 const ruleTester = new RuleTester({
-  parser: '@angular-eslint/template-parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  parser: '@angular-eslint/template-parser'
 });
 
-ruleTester.run(RULE_NAME, rule, {
-  valid,
-  invalid,
+ruleTester.run(rule.ruleName, rule, {
+  valid: [
+    {
+      template: `
+        <button (click)="onClick()">Click me</button>
+      `
+    },
+    {
+      template: `
+        <input (keyup.enter)="onEnterKeyUp()">
+      `
+    }
+  ],
+  invalid: [
+    {
+      template: `
+        <button (click)="onClick()">Click me</button>
+      `,
+      errors: [
+        {
+          messageId: 'mouseEventWithoutKeyEvent',
+          data: { eventName: 'click' },
+          type: 'EventBinding'
+        }
+      ]
+    }
+  ]
 });
