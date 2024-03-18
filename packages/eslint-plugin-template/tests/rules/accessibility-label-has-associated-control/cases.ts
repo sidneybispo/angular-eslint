ba2576f49405752bf73e549089514b53c857b612
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:11 -0300

    improve

--- packages/eslint-plugin-template/tests/rules/accessibility-label-has-associated-control/cases.ts
+++ packages/eslint-plugin-template/tests/rules/accessibility-label-has-associated-control/cases.ts
@@ -3,80 +3,66 @@ import type { MessageIds } from '../../../src/rules/accessibility-label-has-asso
 
 const messageId: MessageIds = 'accessibilityLabelHasAssociatedControl';
 
-export const valid = [
-  `
+export const validExamples = {
+  'template-with-ngFor': `
     <ng-container *ngFor="let item of items; index as index">
-      <label for="item-{{index}}">Label #{{index}</label>
+      <label for="item-{{index}}">Label #{{index}}</label>
       <input id="item-{{index}}" [(ngModel)]="item.name">
     </ng-container>
     <label for="id"></label>
-    <label for="{{id}}"></label>
+    <label [for]="id"></label>
     <label [attr.for]="id"></label>
     <label [htmlFor]="id"></label>
-    `,
-  {
-    code: `
-      <app-label id="name"></app-label>
-      <app-label id="{{name}}"></app-label>
-      <app-label [id]="name"></app-label>
-      <label [htmlFor]="id"></label>
-      `,
-    options: [
-      {
-        controlComponents: ['app-input'],
-        labelComponents: [{ inputs: ['id'], selector: 'app-label' }],
-      },
-    ],
-  },
-  {
-    code: `
-      <label><input type="radio"></label>
-      <label><meter></meter></label>
-      <label><output></output></label>
-      <label><progress></progress></label>
-      <label><select><option>1</option></select></label>
-      <label><textarea></textarea></label>
-      <a-label><input></a-label>
-      <label>
-        Label
-        <input>
-      </label>
-      <label>
-        Label
-        <span><input></span>
-      </label>
-      <app-label>
-        <span>
-          <app-input></app-input>
-        </span>
-      </app-label>
-      `,
-    options: [
-      {
-        controlComponents: ['app-input'],
-        labelComponents: [{ inputs: ['id'], selector: 'app-label' }],
-      },
-    ],
-  },
-];
+  `,
+  'template-with-appLabel': `
+    <app-label id="name"></app-label>
+    <app-label [id]="name"></app-label>
+    <label [htmlFor]="id"></label>
+  `,
+  'template-with-wrapped-input': `
+    <label>
+      Label
+      <input>
+    </label>
+    <label>
+      Label
+      <span><input></span>
+    </label>
+    <app-label>
+      <span>
+        <app-input></app-input>
+      </span>
+    </app-label>
+  `,
+  'template-with-native-form-controls': `
+    <label><input type="radio"></label>
+    <label><meter></meter></label>
+    <label><output></output></label>
+    <label><progress></progress></label>
+    <label><select><option>1</option></select></label>
+    <label><textarea></textarea></label>
+  `,
+  'template-with-custom-component': `
+    <a-label><input></a-label>
+  `,
+};
 
-export const invalid = [
-  convertAnnotatedSourceToFailureCase({
+export const invalidExamples = {
+  'label-without-for-attribute': convertAnnotatedSourceToFailureCase({
     messageId,
     description: 'should fail if a label does not have a "for" attribute',
     annotatedSource: `
-        <label>Label</label>
-        ~~~~~~~~~~~~~~~~~~~~
-      `,
+      <label>Label</label>
+      ~~~~~~~~~~~~~~~~~~~~
+    `,
   }),
-  convertAnnotatedSourceToFailureCase({
+  'appLabel-without-label-attribute': convertAnnotatedSourceToFailureCase({
     messageId,
-    description:
-      'should fail if a label component does not have a label attribute',
+    description: 'should fail if a label component does not have a label attribute',
     annotatedSource: `
-        <app-label anotherAttribute="id"></app-label>
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      `,
+      <app-label anotherAttribute="id"></app-label>
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    `,
     options: [{ labelComponents: [{ inputs: ['id'], selector: 'app-label' }] }],
   }),
-];
+};
