98ca6fd1ef9e20c1b55c2fd59ad35469c583dcba
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:27 -0300

    improve

--- packages/integration-tests/fixtures/v1123-multi-project-manual-config/projects/ice-cream-monster/src/app/app.component.ts
+++ packages/integration-tests/fixtures/v1123-multi-project-manual-config/projects/ice-cream-monster/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ice-cream-monster';
+  title = 'Ice Cream Monster';
+  iceCreamFlavors = ['Vanilla', 'Chocolate', 'Strawberry', 'Mint Chocolate Chip'];
+
+  addIceCreamFlavor(flavor: string) {
+    this.iceCreamFlavors.push(flavor);
+  }
 }
