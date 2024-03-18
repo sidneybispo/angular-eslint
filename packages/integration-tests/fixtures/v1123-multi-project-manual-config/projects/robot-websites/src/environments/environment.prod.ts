4d706a2e5dcdae46be2aff458f581529ad8dfadd
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:31 -0300

    improve

--- packages/integration-tests/fixtures/v1123-multi-project-manual-config/projects/robot-websites/src/environments/environment.prod.ts
+++ packages/integration-tests/fixtures/v1123-multi-project-manual-config/projects/robot-websites/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://my-api-production.com', // replace with your production API URL
+  firebaseConfig: {
+    apiKey: 'your-production-api-key', // replace with your production Firebase API key
+    authDomain: 'your-production-auth-domain', // replace with your production Firebase auth domain
+    databaseURL: 'your-production-database-url', // replace with your production Firebase database URL
+    projectId: 'your-production-project-id', // replace with your production Firebase project ID
+    storageBucket: 'your-production-storage-bucket', // replace with your production Firebase storage bucket
+    messagingSenderId: 'your-production-messaging-sender-id', // replace with your production Firebase messaging sender ID
+    appId: 'your-production-app-id' // replace with your production Firebase app ID
+  }
 };
