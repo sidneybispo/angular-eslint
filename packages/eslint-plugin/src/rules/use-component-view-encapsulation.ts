929501933b7103aaf017ea4ccd8089378df5f122
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:12 -0300

    improve

--- packages/eslint-plugin/src/rules/use-component-view-encapsulation.ts
+++ packages/eslint-plugin/src/rules/use-component-view-encapsulation.ts
@@ -6,27 +6,28 @@ import {
 } from '@angular-eslint/utils';
 import type { TSESTree } from '@typescript-eslint/experimental-utils';
 import { createESLintRule } from '../utils/create-eslint-rule';
+import { ViewEncapsulation } from '@angular/core';
 
 type Options = [];
 export type MessageIds =
   | 'useComponentViewEncapsulation'
   | 'suggestRemoveViewEncapsulationNone';
 export const RULE_NAME = 'use-component-view-encapsulation';
-const VIEW_ENCAPSULATION_NONE = 'ViewEncapsulation.None';
+const VIEW_ENCAPSULULATION_NONE = ViewEncapsulation.None;
 
 export default createESLintRule<Options, MessageIds>({
   name: RULE_NAME,
   meta: {
     type: 'suggestion',
     docs: {
-      description: `Disallows using \`${VIEW_ENCAPSULATION_NONE}\``,
+      description: `Disallows using \`${VIEW_ENCAPSULULATION_NONE}\``,
       recommended: false,
     },
     hasSuggestions: true,
     schema: [],
     messages: {
-      useComponentViewEncapsulation: `Using \`${VIEW_ENCAPSULATION_NONE}\` makes your styles global, which may have an unintended effect`,
-      suggestRemoveViewEncapsulationNone: `Remove \`${VIEW_ENCAPSULATION_NONE}\``,
+      useComponentViewEncapsulation: `Using \`${VIEW_ENCAPSULULATION_NONE}\` makes your styles global, which may have an unintended effect`,
+      suggestRemoveViewEncapsulationNone: `Remove \`${VIEW_ENCAPSULULATION_NONE}\``,
     },
   },
   defaultOptions: [],
@@ -60,7 +61,7 @@ export default createESLintRule<Options, MessageIds>({
                   RuleFixes.getImportRemoveFix(
                     sourceCode,
                     importDeclarations,
-                    'ViewEncapsulation',
+                    ViewEncapsulation.name,
                     fixer,
                   ),
                 ].filter(isNotNullOrUndefined);
