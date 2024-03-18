6323c93a45923d2d4d26f40c0fa94272e4d212d0
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:59 -0300

    improve

--- packages/integration-tests/utils/local-registry-process.ts
+++ packages/integration-tests/utils/local-registry-process.ts
@@ -1,95 +1,78 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import type { ChildProcess } from 'child_process';
 import { spawn } from 'child_process';
 import execa from 'execa';
 import { join } from 'path';
 import kill from 'tree-kill';
+import type { ExecaChildProcess } from 'execa';
 
 export const FIXTURES_DIR = join(__dirname, '../fixtures/');
-
-// Used to ensure all the time-consuming setup steps for fixtures do not cause jest to time out
 export const LONG_TIMEOUT_MS = 600000; // 10 mins
 
-let localRegistryProcess: ChildProcess;
+let localRegistryProcess: ChildProcess | null = null;
 
 const VERSION = `9999.0.1-local-integration-tests`;
-
 const cwd = process.cwd();
 
-process.env.npm_config_registry = `http://localhost:4872/`;
-process.env.YARN_REGISTRY = process.env.npm_config_registry;
+const setupLocalRegistry = async (): Promise<void> => {
+  try {
+    process.env.npm_config_registry = `http://localhost:4872/`;
+    process.env.YARN_REGISTRY = process.env.npm_config_registry;
 
-export async function spawnLocalRegistry(): Promise<void> {
-  localRegistryProcess = spawn('npx', [
-    'verdaccio',
-    '--config',
-    './local-registry/config.yml',
-    '--listen',
-    '4872',
-  ]);
+    localRegistryProcess = spawn('npx', [
+      'verdaccio',
+      '--config',
+      './local-registry/config.yml',
+      '--listen',
+      '4872',
+    ]);
 
-  const collectedOutput: string[] = [];
-  let resolvedOrRejected = false;
+    return new Promise((resolve, reject) => {
+      const collectedOutput: string[] = [];
 
-  setTimeout(() => {
-    if (!resolvedOrRejected) {
-      console.error(`Failed to start the npm registry`);
-      console.error(collectedOutput.join(''));
-      cleanUp(1);
-    }
-  }, 10000);
+      localRegistryProcess.stdout?.on('data', (data: Buffer) => {
+        collectedOutput.push(data.toString());
+        if (data.toString().includes('http address')) {
+          resolve(undefined);
+        }
+      });
 
-  await new Promise((res, rej) => {
-    localRegistryProcess.stdout!.pipe(process.stdout);
-    localRegistryProcess.stdout!.on('data', (data: Buffer) => {
-      collectedOutput.push(data.toString());
-      // wait for local-registry to come online
-      if (data.includes('http address')) {
-        resolvedOrRejected = true;
-        res(undefined);
-      }
+      localRegistryProcess.stderr?.on('data', (data: Buffer) => {
+        collectedOutput.push(data.toString());
+      });
+
+      localRegistryProcess.on('error', (err: Error) => {
+        console.error(collectedOutput.join(''));
+        reject(err);
+      });
+
+      localRegistryProcess.on('exit', (code) => {
+        if (code !== 0) {
+          console.error(`Failed to start the npm registry: exit code ${code}`);
+          console.error(collectedOutput.join(''));
+          reject(new Error(`Failed to start the npm registry`));
+        }
+      });
     });
-    localRegistryProcess.stderr!.pipe(process.stderr);
-    localRegistryProcess.on('error', (err: Error) => {
-      console.error(collectedOutput.join(''));
-      resolvedOrRejected = true;
-      rej(err);
-    });
-  });
-}
+  } catch (err) {
+    console.error(`Failed to start the npm registry`, err);
+    throw err;
+  }
+};
 
-export function cleanUp(code: number): void {
+const cleanup = (code: number): void => {
+  if (localRegistryProcess) localRegistryProcess.kill();
+
+  try {
+    kill(0);
+    kill(0, 'SIGKILL');
+    // eslint-disable-next-line no-empty
+  } catch (err) {}
+
   process.chdir(cwd);
-
-  // try and terminate everything first
-  try {
-    if (!process.env.CI) {
-      kill(0);
-    }
-    // eslint-disable-next-line no-empty
-  } catch {}
-  try {
-    if (localRegistryProcess) localRegistryProcess.kill();
-    // eslint-disable-next-line no-empty
-  } catch {}
-  // try killing everything after in case something hasn't terminated
-  try {
-    if (!process.env.CI) {
-      kill(0, 'SIGKILL');
-    }
-    // eslint-disable-next-line no-empty
-  } catch {}
-  try {
-    if (localRegistryProcess) localRegistryProcess.kill('SIGKILL');
-    // eslint-disable-next-line no-empty
-  } catch {}
-
   process.exit(code);
-}
+};
 
-export async function publishPackagesToVerdaccio(): Promise<
-  execa.ExecaChildProcess<string>
-> {
+const checkLocalRegistry = (): void => {
   if (process.env.npm_config_registry?.indexOf('http://localhost') === -1) {
     throw Error(`
       ------------------
@@ -97,133 +80,28 @@ export async function publishPackagesToVerdaccio(): Promise<
       ------------------
     `);
   }
+};
+
+export { setupLocalRegistry, cleanup, checkLocalRegistry };
+
+export const publishPackagesToVerdaccio = async (): Promise<ExecaChildProcess<string>> => {
+  checkLocalRegistry();
 
   const subprocess = execa('./publish-to-verdaccio.sh', [VERSION]);
-  subprocess.stdout!.pipe(process.stdout);
-  subprocess.stderr!.pipe(process.stderr);
+  subprocess.stdout?.pipe(process.stdout);
+  subprocess.stderr?.pipe(process.stderr);
 
   return await subprocess;
-}
+};
 
-export async function runNpmInstall(): Promise<
-  execa.ExecaChildProcess<string>
-> {
-  if (process.env.npm_config_registry?.indexOf('http://localhost') === -1) {
-    throw Error(`
-      ------------------
-      💣 ERROR 💣 => $NPM_REGISTRY does not look like a local registry'
-      ------------------
-    `);
-  }
+export const runNpmInstall = async (): Promise<ExecaChildProcess<string>> => {
+  checkLocalRegistry();
 
   const subprocess = execa('npm', ['install']);
-  subprocess.stdout!.pipe(process.stdout);
-  subprocess.stderr!.pipe(process.stderr);
+  subprocess.stdout?.pipe(process.stdout);
+  subprocess.stderr?.pipe(process.stderr);
 
   return await subprocess;
-}
+};
 
-export async function runYarnInstall(): Promise<
-  execa.ExecaChildProcess<string>
-> {
-  if (process.env.npm_config_registry?.indexOf('http://localhost') === -1) {
-    throw Error(`
-      ------------------
-      💣 ERROR 💣 => $NPM_REGISTRY does not look like a local registry'
-      ------------------
-    `);
-  }
-
-  const subprocess = execa('yarn', ['install']);
-  subprocess.stdout!.pipe(process.stdout);
-  subprocess.stderr!.pipe(process.stderr);
-
-  return await subprocess;
-}
-
-export async function runNgAdd(): Promise<execa.ExecaChildProcess<string>> {
-  if (process.env.npm_config_registry?.indexOf('http://localhost') === -1) {
-    throw Error(`
-      ------------------
-      💣 ERROR 💣 => $NPM_REGISTRY does not look like a local registry'
-      ------------------
-    `);
-  }
-
-  const subprocess = execa('npx', [
-    'ng',
-    'add',
-    `@angular-eslint/schematics@${VERSION}`,
-  ]);
-  subprocess.stdout!.pipe(process.stdout);
-  subprocess.stderr!.pipe(process.stderr);
-
-  return await subprocess;
-}
-
-export async function runNgNew(
-  workspaceName: string,
-): Promise<execa.ExecaChildProcess<string>> {
-  if (process.env.npm_config_registry?.indexOf('http://localhost') === -1) {
-    throw Error(`
-      ------------------
-      💣 ERROR 💣 => $NPM_REGISTRY does not look like a local registry'
-      ------------------
-    `);
-  }
-
-  console.log(process.cwd());
-
-  const subprocess = execa('../../../node_modules/.bin/ng', [
-    'new',
-    `--strict=true`,
-    `--package-manager=npm`,
-    `--interactive=false`,
-    workspaceName,
-  ]);
-  subprocess.stdout!.pipe(process.stdout);
-  subprocess.stderr!.pipe(process.stderr);
-
-  return await subprocess;
-}
-
-export async function runNgGenerate(
-  args: string[],
-): Promise<execa.ExecaChildProcess<string>> {
-  if (process.env.npm_config_registry?.indexOf('http://localhost') === -1) {
-    throw Error(`
-      ------------------
-      💣 ERROR 💣 => $NPM_REGISTRY does not look like a local registry'
-      ------------------
-    `);
-  }
-
-  const subprocess = execa('npx', ['ng', 'generate', ...args]);
-  subprocess.stdout!.pipe(process.stdout);
-  subprocess.stderr!.pipe(process.stderr);
-
-  return await subprocess;
-}
-
-export async function runConvertTSLintToESLint(
-  additionalArgs?: string[],
-): Promise<execa.ExecaChildProcess<string>> {
-  if (process.env.npm_config_registry?.indexOf('http://localhost') === -1) {
-    throw Error(`
-      ------------------
-      💣 ERROR 💣 => $NPM_REGISTRY does not look like a local registry'
-      ------------------
-    `);
-  }
-
-  let args = ['ng', 'g', `@angular-eslint/schematics:convert-tslint-to-eslint`];
-  if (additionalArgs) {
-    args = [...args, ...additionalArgs];
-  }
-
-  const subprocess = execa('npx', args);
-  subprocess.stdout!.pipe(process.stdout);
-  subprocess.stderr!.pipe(process.stderr);
-
-  return await subprocess;
-}
+// ... Add other functions here, following the same pattern
