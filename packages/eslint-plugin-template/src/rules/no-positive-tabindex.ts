7aa3ab6c02dce1b53c92d775dd9291a4b97e6d3c
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:08 -0300

    improve

--- packages/eslint-plugin-template/src/rules/no-positive-tabindex.ts
+++ packages/eslint-plugin-template/src/rules/no-positive-tabindex.ts
@@ -11,8 +11,8 @@ import { getDomElements } from '../utils/get-dom-elements';
 import { toPattern } from '../utils/to-pattern';
 
 type Options = [];
-export type MessageIds = 'noPositiveTabindex' | 'suggestNonNegativeTabindex';
-export const RULE_NAME = 'no-positive-tabindex';
+type MessageIds = 'noPositiveTabindex' | 'suggestNonNegativeTabindex';
+const RULE_NAME = 'no-positive-tabindex';
 
 export default createESLintRule<Options, MessageIds>({
   name: RULE_NAME,
@@ -32,28 +32,26 @@ export default createESLintRule<Options, MessageIds>({
   defaultOptions: [],
   create(context) {
     const parserServices = getTemplateParserServices(context);
-    const elementNamePattern = toPattern([...getDomElements()]);
+    const elementNamePattern = toPattern(getDomElements());
 
     return {
-      [`Element$1[name=${elementNamePattern}] > BoundAttribute[name="tabindex"][value.ast.value>0], TextAttribute[name="tabindex"][value>0]`]({
-        valueSpan,
-      }: (TmplAstBoundAttribute | TmplAstTextAttribute) & {
-        valueSpan: ParseSourceSpan;
-      }) {
-        const loc = parserServices.convertNodeSourceSpanToLoc(valueSpan);
+      // Use template literals for better readability
+      [`Element$1[name=${elementNamePattern}] > BoundAttribute[name="tabindex"][value.ast.value>0], TextAttribute[name="tabindex"][value>0]`](
+        node: TmplAstBoundAttribute | TmplAstTextAttribute & { valueSpan: ParseSourceSpan },
+      ) {
+        const loc = parserServices.convertNodeSourceSpanToLoc(node.valueSpan);
 
         context.report({
           loc,
           messageId: 'noPositiveTabindex',
-          suggest: ['-1', '0'].map((tabindex) => ({
-            messageId: 'suggestNonNegativeTabindex',
-            fix: (fixer) =>
-              fixer.replaceTextRange(
-                [valueSpan.start.offset, valueSpan.end.offset],
-                tabindex,
-              ),
-            data: { tabindex },
-          })),
+          suggest: [
+            // Use destructuring to get `tabindex` directly
+            ...['-1', '0'].map((tabindex) => ({
+              messageId: 'suggestNonNegativeTabindex',
+              fix: (fixer) => fixer.replaceTextRange([node.valueSpan.start.offset, node.valueSpan.end.offset], tabindex),
+              data: { tabindex },
+            })),
+          ],
         });
       },
     };
