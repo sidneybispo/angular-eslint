597a839f20d043208a6009cab3b549fe24d0f6b4
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:45 -0300

    improve

--- packages/integration-tests/fixtures/v1123-single-project-yarn-auto-convert/src/app/app.component.spec.ts
+++ packages/integration-tests/fixtures/v1123-single-project-yarn-auto-convert/src/app/app.component.spec.ts
@@ -2,29 +2,30 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'v1123-single-project-yarn-auto-convert'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('v1123-single-project-yarn-auto-convert');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have the correct title', () => {
+    expect(component.title).toEqual('v1123-single-project-yarn-auto-convert');
+  });
+
+  it('should render the title in the page', () => {
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('v1123-single-project-yarn-auto-convert app is running!');
   });
