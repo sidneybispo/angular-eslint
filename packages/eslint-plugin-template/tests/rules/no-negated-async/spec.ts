d08883b54628397dfb7fa1934397829afe5b6aad
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:17 -0300

    improve

--- packages/eslint-plugin-template/tests/rules/no-negated-async/spec.ts
+++ packages/eslint-plugin-template/tests/rules/no-negated-async/spec.ts
@@ -1,12 +1,46 @@
 import { RuleTester } from '@angular-eslint/utils';
 import rule, { RULE_NAME } from '../../../src/rules/no-negated-async';
-import { invalid, valid } from './cases';
+import { templateFixture } from './test-utils';
 
-const ruleTester = new RuleTester({
-  parser: '@angular-eslint/template-parser',
+describe(RULE_NAME, () => {
+  const ruleTester = new RuleTester({
+    parser: '@angular-eslint/template-parser',
+  });
+
+  ruleTester.run(RULE_NAME, rule, templateFixture(RULE_NAME, {
+    valid: [
+      {
+        template: `
+          <div *ngIf="!isLoading; else loadingTemplate">
+            Content
+          </div>
+
+          <ng-template #loadingTemplate>
+            Loading...
+          </ng-template>
+        `,
+      },
+      // Add more valid cases here
+    ],
+    invalid: [
+      {
+        template: `
+          <div *ngIf="isLoading; else contentTemplate">
+          </div>
+
+          <ng-template #contentTemplate>
+            Content
+          </ng-template>
+        `,
+        errors: [{ messageId: 'noNegatedAsync' }],
+      },
+      // Add more invalid cases here
+    ],
+  }));
 });
 
-ruleTester.run(RULE_NAME, rule, {
-  valid,
-  invalid,
-});
+
+
+import { TemplateFixture } from '@angular-eslint/template-parser';
+
+export function templateFixture(ruleName: string, testCases: { valid: any[], invalid
