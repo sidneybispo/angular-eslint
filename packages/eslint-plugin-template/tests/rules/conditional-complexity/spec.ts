33b7df8c4e2673e1d26bbf9e36f2857c22f384b6
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:17 -0300

    improve

--- packages/eslint-plugin-template/tests/rules/conditional-complexity/spec.ts
+++ packages/eslint-plugin-template/tests/rules/conditional-complexity/spec.ts
@@ -1,12 +1,43 @@
 import { RuleTester } from '@angular-eslint/utils';
 import rule, { RULE_NAME } from '../../../src/rules/conditional-complexity';
-import { invalid, valid } from './cases';
+import { templateStringTo Ast } from '@angular-eslint/template-parser';
 
 const ruleTester = new RuleTester({
-  parser: '@angular-eslint/template-parser',
+  parserOptions: {
+    parser: '@angular-eslint/template-parser',
+  },
 });
 
+const parseTemplate = (template: string) => templateStringTo Ast(template);
+
+const validTemplate = `
+  <div *ngIf="condition; else elseBlock">
+    Content to render when condition is true
+  </div>
+  <ng-template #elseBlock>
+    Content to render when condition is false
+  </ng-template>
+`;
+
+const invalidTemplate = `
+  <div *ngIf="complexCondition1 && complexCondition2 || complexCondition3; else elseBlock">
+    Content to render when complex condition is true
+  </div>
+  <ng-template #elseBlock>
+    Content to render when complex condition is false
+  </ng-template>
+`;
+
 ruleTester.run(RULE_NAME, rule, {
-  valid,
-  invalid,
+  valid: [
+    {
+      code: parseTemplate(validTemplate),
+    },
+  ],
+  invalid: [
+    {
+      code: parseTemplate(invalidTemplate),
+      errors: [{ messageId: 'tooComplex', data: { maxNesting: 1 } }],
+    },
+  ],
 });
