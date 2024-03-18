78764076cd80a7e0a3b924b741339b9fc50081af
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:28 -0300

    improve

--- packages/eslint-plugin/tests/rules/use-lifecycle-interface/cases.ts
+++ packages/eslint-plugin/tests/rules/use-lifecycle-interface/cases.ts
@@ -1,110 +1,69 @@
 import {
   ASTUtils,
   convertAnnotatedSourceToFailureCase,
+  MessageIds,
 } from '@angular-eslint/utils';
-import type { MessageIds } from '../../../src/rules/use-lifecycle-interface';
+import type { Component, Directive, Injectable, NgModule } from '@angular/core';
+import type { DoBootstrap } from '@angular/core/src/linker/element_ref';
 
 const messageId: MessageIds = 'useLifecycleInterface';
 
-export const valid = [
-  `
-    class Test implements OnInit {
-      ngOnInit() {}
-    }
-    `,
-  `class Test implements DoBootstrap {
-      ngDoBootstrap() {}
-    }
-    `,
-  `
-    class Test extends Component implements OnInit, OnDestroy  {
-      ngOnInit() {}
-
-      private ngOnChanges = '';
-
-      ngOnDestroy() {}
-
-      ngOnSmth() {}
-    }
-    `,
-  `
-    class Test extends Component implements ng.OnInit, ng.OnDestroy  {
-      ngOnInit() {}
-
-      private ngOnChanges = '';
-
-      ngOnDestroy() {}
-
-      ngOnSmth() {}
-    }
-    `,
-  'class Test {}',
-];
-
-export const invalid = [
-  convertAnnotatedSourceToFailureCase({
-    description:
-      'it should fail if lifecycle method is declared without implementing its interface',
-    annotatedSource: `
-        @Component()
-        class Test {
-          ngOnInit() {
-          ~~~~~~~~
-          }
-        }
-      `,
-    messageId,
-    data: {
-      interfaceName: ASTUtils.AngularLifecycleInterfaces.OnInit,
-      methodName: ASTUtils.AngularLifecycleMethods.ngOnInit,
-    },
-  }),
-  convertAnnotatedSourceToFailureCase({
-    description:
-      'it should fail if one of the lifecycle methods is declared without implementing its interface',
-    annotatedSource: `
-        @Directive()
-        class Test extends Component implements OnInit {
+describe('useLifecycleInterface rule', () => {
+  describe('valid cases', () => {
+    const valid = [
+      `
+        class Test implements OnInit {
           ngOnInit() {}
-
-          ngOnDestroy() {
-          ~~~~~~~~~~~
-          }
         }
-      `,
-    messageId,
-    data: {
-      interfaceName: ASTUtils.AngularLifecycleInterfaces.OnDestroy,
-      methodName: ASTUtils.AngularLifecycleMethods.ngOnDestroy,
-    },
-  }),
-  convertAnnotatedSourceToFailureCase({
-    description:
-      'it should fail if lifecycle methods are declared without implementing their interfaces',
-    annotatedSource: `
-        @Injectable()
-        class Test {
+        `,
+      `class Test implements DoBootstrap {
           ngDoBootstrap() {}
-          ~~~~~~~~~~~~~
-
+        }
+        `,
+      `
+        class Test extends Component implements OnInit, OnDestroy  {
           ngOnInit() {}
-          ^^^^^^^^
+
+          private ngOnChanges = '';
 
           ngOnDestroy() {}
-          ###########
+
+          ngOnSmth() {}
         }
-      `,
-    messages: [
+        `,
+      `
+        class Test extends Component implements ng.OnInit, ng.OnDestroy  {
+          ngOnInit() {}
+
+          private ngOnChanges = '';
+
+          ngOnDestroy() {}
+
+          ngOnSmth() {}
+        }
+        `,
+      'class Test {}',
+    ];
+
+    valid.forEach(code => {
+      it(`should not fail for valid code: ${code}`, () => {
+        expect(convertAnnotatedSourceToFailureCase({ annotatedSource: code })).toBeUndefined();
+      });
+    });
+  });
+
+  describe('invalid cases', () => {
+    const invalid = [
       {
-        char: '~',
-        messageId,
-        data: {
-          interfaceName: ASTUtils.AngularLifecycleInterfaces.DoBootstrap,
-          methodName: ASTUtils.AngularLifecycleMethods.ngDoBootstrap,
-        },
-      },
-      {
-        char: '^',
+        description: 'it should fail if lifecycle method is declared without implementing its interface',
+        annotatedSource: `
+            @Component()
+            class Test {
+              ngOnInit() {
+              ~~~~~~~~
+              }
+            }
+          `,
         messageId,
         data: {
           interfaceName: ASTUtils.AngularLifecycleInterfaces.OnInit,
@@ -112,32 +71,96 @@ export const invalid = [
         },
       },
       {
-        char: '#',
+        description: 'it should fail if one of the lifecycle methods is declared without implementing its interface',
+        annotatedSource: `
+            @Directive()
+            class Test extends Component implements OnInit {
+              ngOnInit() {}
+
+              ngOnDestroy() {
+              ~~~~~~~~~~~
+              }
+            }
+          `,
         messageId,
         data: {
           interfaceName: ASTUtils.AngularLifecycleInterfaces.OnDestroy,
           methodName: ASTUtils.AngularLifecycleMethods.ngOnDestroy,
         },
       },
-    ],
-  }),
-  convertAnnotatedSourceToFailureCase({
-    description:
-      'it should fail if lifecycle methods are declared without implementing their interfaces, using namespace',
-    annotatedSource: `
-        @NgModule()
-        class Test extends Component implements ng.OnInit {
-          ngOnInit() {}
+      {
+        description: 'it should fail if lifecycle methods are declared without implementing their interfaces',
+        annotatedSource: `
+            @Injectable()
+            class Test {
+              ngDoBootstrap() {}
+              ~~~~~~~~~~~~~
 
-          ngOnDestroy() {
-          ~~~~~~~~~~~
-          }
-        }
-      `,
-    messageId,
-    data: {
-      interfaceName: ASTUtils.AngularLifecycleInterfaces.OnDestroy,
-      methodName: ASTUtils.AngularLifecycleMethods.ngOnDestroy,
-    },
-  }),
-];
+              ngOnInit() {}
+              ^^^^^^^^
+
+              ngOnDestroy() {}
+              ###########
+            }
+          `,
+        messages: [
+          {
+            char: '~',
+            messageId,
+            data: {
+              interfaceName: ASTUtils.AngularLifecycleInterfaces.DoBootstrap,
+              methodName: ASTUtils.AngularLifecycleMethods.ngDoBootstrap,
+            },
+          },
+          {
+            char: '^',
+            messageId,
+            data: {
+              interfaceName: ASTUtils.AngularLifecycleInterfaces.OnInit,
+              methodName: ASTUtils.AngularLifecycleMethods.ngOnInit,
+            },
+          },
+          {
+            char: '#',
+            messageId,
+            data: {
+              interfaceName: ASTUtils.AngularLifecycleInterfaces.OnDestroy,
+              methodName: ASTUtils.AngularLifecycleMethods.ngOnDestroy,
+            },
+          },
+        ],
+      },
+      {
+        description: 'it should fail if lifecycle methods are declared without implementing their interfaces, using namespace',
+        annotatedSource: `
+            @NgModule()
+            class Test extends Component implements ng.OnInit {
+              ngOnInit() {}
+
+              ngOnDestroy() {
+              ~~~~~~~~~~~
+              }
+            }
+          `,
+        messageId,
+        data: {
+          interfaceName: ASTUtils.AngularLifecycleInterfaces.OnDestroy,
+          methodName: ASTUtils.AngularLifecycleMethods.ngOnDestroy,
+        },
+      },
+    ];
+
+    invalid.forEach(({ description, annotatedSource, messageId, data, messages }) => {
+      it(description, () => {
+        const failureCase = convertAnnotatedSourceToFailureCase({
+          annotatedSource,
+          messageId,
+          data,
+          messages,
+        });
+        expect(failureCase).toBeDefined();
+      });
+    });
+  });
+});
+
