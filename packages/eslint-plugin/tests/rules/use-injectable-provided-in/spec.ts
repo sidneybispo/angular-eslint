cff5024e8f718126160b25aee4f8eb106a93a64a
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2024-03-18 13:04:20 -0300

    improve

--- packages/eslint-plugin/tests/rules/use-injectable-provided-in/spec.ts
+++ packages/eslint-plugin/tests/rules/use-injectable-provided-in/spec.ts
@@ -1,9 +1,15 @@
 import { RuleTester } from '@angular-eslint/utils';
-import rule, { RULE_NAME } from '../../../src/rules/use-injectable-provided-in';
+import { RULE_NAME, rule } from '../../../src/rules/use-injectable-provided-in';
 import { invalid, valid } from './cases';
 
 const ruleTester = new RuleTester({
-  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
 });
 
 ruleTester.run(RULE_NAME, rule, {
